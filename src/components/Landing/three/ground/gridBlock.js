58a03fca39e7417d61797be944e995dac57a0cb7
Juha Kangas <juhakangas5@gmail.com>
2020-06-08 11:03:53 +0300

    added mouse press and hover option

--- src/components/Landing/three/ground/gridBlock.js
+++ src/components/Landing/three/ground/gridBlock.js
@@ -7,17 +7,31 @@ export default function GridBlock(props) {
     const [color, setColor] = useState('orange')
     const [hover, setHover] = useState(false)
     const [selected, setSelected] = useState(false)
+    const [keyDown,setKeyDown] = useState(false)
 
+
+    const selectBlocks=(e)=>{
+        setHover(true)
+        if(e.buttons===1){
+            setSelected(true)
+        }
+    }
+
+    console.log(keyDown);
     return (
         <mesh
-        onClick={e => setSelected(!selected)}
-        onPointerOver={e => setHover(true)}
+        onClick={e => 
+            setSelected(!selected)
+        }
+        onPointerOver={e => selectBlocks(e)}
         onPointerOut={e => setHover(false)}
+        onKeyDown={e => setKeyDown(true)}
+
         {...props}
         ref={mesh}
         scale={[1.5, 1.5, 1.5] }>
         <boxBufferGeometry attach="geometry" args={[10, 10, hover||selected?5:1]} />
-        <meshStandardMaterial attach="material" color={selected?'green':hover?'blue':'orange'} />
+        <meshStandardMaterial attach="material" color={selected?'green':hover?'red':'orange'} />
       </mesh>
     )
 }
